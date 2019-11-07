4c110cc92abe743a21af6c938602f13e92e17ddb
Natte <nattea>
2019-11-07 21:03:20 +0700

    Initial Commit

---
+++ MyMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMoney
+//
+//  Created by Natte Vagon on 07/11/19.
+//  Copyright © 2019 NatteVagon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
